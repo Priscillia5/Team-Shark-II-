68d3489f308263abad6b7494a6e0f86dcedff73c
Priscillia Archibong <archibongpriscillia@gmail.com>
2022-12-03 16:56:56 +0100

    setup api app

--- col_films/settings.py
+++ col_films/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Import api app
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
