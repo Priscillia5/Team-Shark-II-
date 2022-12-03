78ea3676aa8e794551a78fe0e5b51fef20db56ce
Priscillia Archibong <archibongpriscillia@gmail.com>
2022-12-03 16:57:32 +0100

    create and configured model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class Movies(models.Model):
+    id = models.IntegerField(primary_key = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 100)
+    url_path = models.CharField(max_length = 200)
+    description = models.TextField(null = True, blank = True)
+    duration = models.IntegerField()
+    updated = models.DateTimeField(auto_now = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    movies = models.ForeignKey(Movies, on_delete = models.CASCADE)
+    text = models.TextField(null = True, blank = True)
+
+class React(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    movies = models.ForeignKey(Movies, on_delete = models.CASCADE)
+
+class Share(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    movies = models.ForeignKey(Movies, on_delete = models.CASCADE)
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    movies = models.ForeignKey(Movies, on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
